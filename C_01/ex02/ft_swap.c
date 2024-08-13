e25fd77983a956f502e3f39fb06297e62638db2d
djsurgeon <djsurgeon83@gmail.com>
2024-08-13 17:03:27 +0200

    Norminette in C_01

--- C_01/ex02/ft_swap.c
+++ C_01/ex02/ft_swap.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_swap.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: DjSurgeon <djsurgeon83@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/13 16:56:28 by DjSurgeon         #+#    #+#             */
+/*   Updated: 2024/08/13 16:56:43 by DjSurgeon        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdio.h>
 
 void	ft_swap(int *a, int *b);
@@ -18,9 +30,8 @@ int	main(void)
 
 	a = 24;
 	b = 33;
-
 	printf("El valor de a es: %i y b: %i\n", a, b);
 	ft_swap(&a, &b);
 	printf("El nuevo valor de a es: %i y b: %i", a, b);
-	return(0);
+	return (0);
 }
--- C_01/ex07/ft_rev_int_tab.c
+++ C_01/ex07/ft_rev_int_tab.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: DjSurgeon <djsurgeon83@gmail.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/13 16:57:16 by DjSurgeon         #+#    #+#             */
+/*   Updated: 2024/08/13 16:58:08 by DjSurgeon        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void	ft_rev_int_tab(int *tab, int size);
@@ -21,14 +33,13 @@ void	ft_rev_int_tab(int *tab, int size)
 	}
 }
 
-int main(void)
+int main (void)
 {
 	int	size;
-	int 	tab[size];
+	int	tab[size];
 
 	size = 6;
 	tab[size] = {10, 52, 35, 61, 7, 47};
-
 	ft_rev_int_tab(tab, size);
-	return(0);
+	return (0);
 }
